a74fe6c50ef4b51492ffdd470f2d1e78e1c9def2
YY <37497289+avt1745zet@users.noreply.github.com>
2022-01-20 16:01:46 +0800

    Enforce performance

--- typescript/6-Zigzag Conversion.ts
+++ typescript/6-Zigzag Conversion.ts
@@ -6,30 +6,33 @@
 
 // @lc code=start
 function convert ( s: string, numRows: number ): string {
-	let result: string = '';
-	const arr2d: Array<Array<string>> = new Array<Array<string>>();
-	for ( let i: number = 0; i < s.length; i++ ) {
-		//* cut head & foot
-		const middleCount: number = numRows >= 2 ? numRows - 2 : 0;
-		const groupCount: number = numRows + middleCount;
-		const groupWidth: number = numRows >= 2 ? numRows - 1 : numRows;
-		const quotient: number = Math.floor( i / groupCount );
-		const remainder: number = i % groupCount;
-
-		const column: number =
-			quotient * groupWidth + ( remainder > numRows - 1 ? remainder - numRows + 1 : 0 );
-		const row: number =
-			remainder < numRows ? remainder : ( numRows - 2 ) - ( remainder - numRows );
-		if ( !arr2d[ column ] ) {
-			arr2d[ column ] = new Array<string>();
-		}
-		arr2d[ column ][ row ] = s[ i ];
+	if ( numRows === 1 ) {
+		return s;
 	}
-
+	let result: string = '';
 	for ( let row: number = 0; row < numRows; row++ ) {
-		for ( let column: number = 0; column < arr2d.length; column++ ) {
-			if ( arr2d[ column ][ row ] ) {
-				result += arr2d[ column ][ row ];
+		let belowAngle: number = ( numRows - 1 - row ) * 2;
+		let upperAngle: number = row * 2;
+
+		if ( belowAngle === 0 ) {
+			belowAngle = upperAngle;
+		}
+		if ( upperAngle === 0 ) {
+			upperAngle = belowAngle;
+		}
+
+		let curAngle: number = NaN;
+		for ( let index: number = row; index < s.length; index += curAngle ) {
+			result += s[ index ];
+
+			if ( isNaN( curAngle ) ) {
+				curAngle = belowAngle;
+			} else {
+				if ( curAngle === belowAngle ) {
+					curAngle = upperAngle;
+				} else {
+					curAngle = belowAngle;
+				}
 			}
 		}
 	}
